aec71f8b9383fbfc4834c6e68aaf198fb30cc16b
NikitaSliunin <nikitajke19@gmail.com>
2024-02-28 17:02:29 +0300

    admin auth

---
+++ db/seeders/20240228135137-admin.js
@@ -0,0 +1,25 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'Admins',
+      [
+        {
+          login: 'admin',
+          password: 'admin',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
